c567119cf2531e755e23d45862b47f4baed6766c
FedericoSpina <FedericoSpina@hotmail.com>
2022-10-15 23:26:41 -0300

    Banner incial para ir charlandolo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+/* import Todo from "./Index App/Todo/Todo"; */
+import Banner from "./Index App/Banner/Banner";
+
+function App() {
+  return (
+    <>
+      <Banner/>
+    </>
+  );
+}
+
+export default App;
